014ec3e6113bd6deac44595013c916165753c033
Marlene Guzman <41800372+mardb@users.noreply.github.com>
2022-07-29 14:13:50 -0700

    wip

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,16 +2,32 @@ import React from "react";
 import {Link} from 'react-router-dom'
 //make stateless component
 const Header = () =>{
+  const that = useContext(Context.Context)
+  const authUser = that.authenticatedUser;
 
   return(
     <header>
         <div className="wrap header--flex">
             <h1 className="header--logo"><a to={'/'}>Courses</a></h1>
             <nav> 
-                <ul className="header--signedout">
-                    <li><a to={'/signup'}>Sign Up</a></li>
-                    <li><a to={'/signin'}>Sign In</a></li>
+              {authUser ? (
+                <React.Fragment>
+                <span>Welcome, {authUser.name}!</span>
+                <Link to="/signout">Sign Out</Link>
+              </React.Fragment>
+                ) : (
+                <React.Fragment>
+                  <ul className="header--signedout">
+                    <Link to={'/signup'}>Sign Up</Link>
+                    <Link to={'/signin'}>Sign In</Link>
                 </ul>
+                </React.Fragment>
+
+              ) }
+
+              )
+            }
+            
             </nav>
         </div>
     </header>

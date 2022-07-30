0fa4cbb90be68c2ccc90aeb5cf63fc924f1b5cfd
Marlene Guzman <41800372+mardb@users.noreply.github.com>
2022-07-30 13:57:30 -0700

    latest

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -5,11 +5,22 @@ import { Context } from '../Context';
 //make stateless component later
 const Header = (props) => {
   // const that = useContext(Context.Context)
-  // const { data, actions, authenticatedUser, createUser, context } = useContext(Context);
+  const { data, 
+    actions, 
+    authenticatedUser, 
+    // createUser, 
+    // context 
+  } = useContext(Context);
   // console.log(useContext(Context));
-  // const authUser = data.authenticatedUser;
-  // console.log(useContext(Context));
-  // console.log(authUser);
+  const authUser = authenticatedUser;
+  console.log({data, 
+    actions, 
+    authenticatedUser, 
+    authUser, 
+    actionsAuthUser:actions.authenticatedUser});
+  console.log(useContext(Context));
+  console.log(authUser);
+  
   return (
     <header>
       <div className="wrap header--flex">
@@ -18,14 +29,14 @@ const Header = (props) => {
         </h1>
         <nav>
           <ul className="header--signedout">
-            {/* {authUser ? ( */}
+            {authUser ? (
               <React.Fragment>
-                {/* <span> Welcome, {authUser.firstName}!</span> */}
+                <span> Welcome, {authUser.firstName}!</span>
                 <li>
                   <Link to={'/signout'}>Sign Out</Link>
                 </li>
               </React.Fragment>
-        {/* ) : ( */}
+        ) : ( 
               <React.Fragment>
                 <li>
                   <Link to="/signup">Sign Up</Link>
@@ -34,7 +45,7 @@ const Header = (props) => {
                   <Link to="/signin">Sign In</Link>
                 </li>
               </React.Fragment>
-          {/* )}  */}
+       )}  
           </ul>
         </nav>
       </div>

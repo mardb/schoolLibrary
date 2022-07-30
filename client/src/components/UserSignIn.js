9e95407d3ea9a92a6dc422fc3953965d82c19b1d
Marlene Guzman <41800372+mardb@users.noreply.github.com>
2022-07-30 11:01:54 -0700

    adding latest

--- client/src/components/UserSignIn.js
+++ client/src/components/UserSignIn.js
@@ -1,61 +1,89 @@
-import React, {Component} from "react";
-import {Link, useHistory} from 'react-router-dom'
-import Data from '../Data'
-import { Context } from "../Context";
+import React, { Component, useContext, useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import Data from '../Data';
+import { Context } from '../Context';
 
-const UserSignIn = () =>{
+const UserSignIn = () => {
   const history = useHistory();
-  const { data, actions, authenticatedUser, createUser, context } = useContext(Context);
+  // const { data, actions, authenticatedUser, createUser, context } = useContext(Context);
 
   const [user, setUser] = useState({
-    emailAddress: '',
-    password: '',
-    errors:[]
+  //   emailAddress: '',
+  //   password: '',
+  //   errors:[]
   });
-  const [isLoading, setIsLoading] = useState(true);
-
-
+  // const [isLoading, setIsLoading] = useState(true);
 
   const submit = () => {
-   
-    const {context} = this.props;
-    const {username, password} = this.state;
-    context.actions.signIn(username, password).then(user => {
-      if(user === null){
-        this.setState(() => {
-          return {errors:  [ 'Sign-in was unsuccessful']};
-        });
-      } else {
-        history.push('/authenticated')
-        console.log(`SUCCESS! ${username} is now signed in!`);
-      }
-    }).catch(err => {
-      console.log(err);
-      history.push('/error')
-    })
-  }
+    const { context } = this.props;
+    const { username, password } = this.state;
+    context.actions
+      .signIn(username, password)
+      .then((user) => {
+        if (user === null) {
+          this.setState(() => {
+            return { errors: ['Sign-in was unsuccessful'] };
+          });
+        } else {
+          history.push('/authenticated');
+          console.log(`SUCCESS! ${username} is now signed in!`);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        history.push('/error');
+      });
+  };
 
-  const cancel = (e) =>{
-  e.preventDefault()
-    history.push('/')
-  }
+  const cancel = (e) => {
+    e.preventDefault();
+    history.push('/');
+  };
 
-  return(
+  return (
     <div className="form--centered">
-                <h2>Sign In</h2>
-                
-                <form cancel=''  > 
-                    <label htmlFor="emailAddress">Email Address</label>
-                    <input id="emailAddress" name="emailAddress" type="email" value=""/>
-                    <label htmlFor="password">Password</label>
-                    <input id="password" name="password" type="password" value=""/>
-                    <button className="button" type="submit" onClick={submit}>Sign In</button>
-                    <button className="button button-secondary" onClick={cancel} to={'/'}>Cancel</button>
-                </form>
-                <p>Don't have a user account? Click here to <a to={'/signup'}>sign up</a>!</p>
-                
-            </div>
-  )
-}
+      <h2>Sign In</h2>
+
+      <form cancel="">
+        <label htmlFor="emailAddress">Email Address</label>
+        <input
+          id="emailAddress"
+          name="emailAddress"
+          type="email"
+          value={user.emailAddress}
+          onChange={(e) => {
+            setUser({
+              ...user,
+              emailAddress: e.target.value,
+            });
+          }}
+        />
+        <label htmlFor="password">Password</label>
+        <input
+          id="password"
+          name="password"
+          type="password"
+          value={user.password}
+          onChange={(e) => {
+            setUser({
+              ...user,
+              password: e.target.value,
+            });
+          }}
+        />
+        <button className="button" type="submit" onClick={submit}>
+          Sign In
+        </button>
+        <button className="button button-secondary" onClick={cancel} to={'/'}>
+          Cancel
+        </button>
+      </form>
+      <p>
+        Don't have a user account? Click here to{' '}
+        <Link to={'/signup'}>sign up</Link>!
+      </p>
+    </div>
+  );
+};
 
 export default UserSignIn;

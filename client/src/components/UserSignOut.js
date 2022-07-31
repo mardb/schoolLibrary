34e772cbf3fe8e680dc6b20563bd8c04a041bbe4
Marlene Guzman <41800372+mardb@users.noreply.github.com>
2022-07-30 17:30:30 -0700

    user sign out

--- client/src/components/UserSignOut.js
+++ client/src/components/UserSignOut.js
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useHistory } from "react-router-dom";
 import { Redirect } from 'react-router-dom'
 //change to stateless component
 const UserSignOut = ({context}) => {
+const {push} = useHistory();
 
+useEffect(() => {
+  context.actions.UserSignOut();
+  push('/')
+}, [])
   return(
-    <></>
+    <> Signing you out...</>
   )
 }
 

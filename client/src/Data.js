89594aed33f12856335c2630b485eabe44b76883
Marlene Guzman <41800372+mardb@users.noreply.github.com>
2022-07-31 23:19:50 -0700

    comment code out just in case

--- client/src/Data.js
+++ client/src/Data.js
@@ -21,8 +21,8 @@ export default class Data {
   //     const encodedCredentials = Buffer.from(`${credentials.emailAddress}:${credentials.password}`).toString('base64');
   //     options.headers['Authorization'] = `Basic ${encodedCredentials}`;
   //   }
-  //   return fetch(url, options);
-  // }
+    return fetch(url, options);
+  }
   
 //  all properties and values for the currently authenticated User 
   async getUser(emailAddress, password) {
